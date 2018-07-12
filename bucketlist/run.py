949a907651726df980bf8789660767428dfbe1a7
liguiyang <work@localhost.localdomain>
2018-07-12 13:40:44 +0800

    cancel bucketlist

--- bucketlist/run.py
+++
@@ -1,6 +0,0 @@
-import os
-from app import create_app
-
-config_name = os.getenv('APP_SETTING')
-app = create_app(config_name)
-
--- bucketlist/views.py
+++
@@ -1,8 +0,0 @@
-
-#from run import app
-
-#@app.route("/")
-#def hello():
-#    return "hello"
-#
-
