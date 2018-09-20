abc16fa27f6d33e75909285911c2b2ea1faaf486
liguiyang <liguiyang@bjdhj-52-131.58os.org>
2018-09-20 13:31:05 +0800

    modify dirs

--- KAndR/ch5/main.c
+++
@@ -1,9 +0,0 @@
-#include "swap.h"
-void main(){
-    int a = 1, b = 9;
-    int *x = &a, *y = &b;
-
-    printf("Before swap: a=%d, b=%d\n", a, b);
-    swap(x,y);
-    printf("After  swap: a=%d, b=%d\n", a, b);
-}
--- KAndR/ch5/swap.c
+++
@@ -1,7 +0,0 @@
-
-void swap(int *x, int *y){
-    int tmp;
-    tmp = *y;
-    *y = *x;
-    *x = tmp;
-}
